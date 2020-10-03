613bce4f7ff1bad51abcb2d8f780d87bd9ce139b
Scott-JYTvr <>
2020-10-03 22:55:56 +0800

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// prod.js - production keys here
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI3: process.env.MONGO_URI3,
+	mongoURI2: process.env.MONGO_URI2,
+	cookieKey: process.env.COOKIE_KEY,
+};
